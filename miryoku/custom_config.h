ac7349408004cf016ecbbf0bf85371552c18db60
Dhruvin Shah <dshah@evertz.com>
2023-03-22 16:15:36 -0400

    trying to add lynx

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_COLEMAKDH
+#define MIRYOKU_TAP_COLEMAKDH
+#define MIRYOKU_EXTRA_QWERTY
+#define MIRYOKU_CLIPBOARD_WIN
